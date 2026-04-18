b38fe3a22b85c9fb442e264aca9e36c7619c4059
SunLee1072 <chlee@knox.edu>
2026-04-18 17:25:02 -0500

    first commit

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("a is bigger than b");
+        } else if (a == b) {
+            System.out.println("a and b is equal");
+        } else {
+            System.out.println("b is bigger than a");
+        }
+    }
+}
